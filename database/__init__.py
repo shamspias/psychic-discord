f69e7136737557db538ffbd73f9a75db781f8a55
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:30:27 +0600

    fix new database

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
+from config.development import CONFIG
 
-DATABASE_URL = "mysql+pymysql://username:password@localhost/dbname"  # Replace with your actual MySQL connection details
-
-engine = create_engine(DATABASE_URL, echo=True)  # echo=True will log SQL queries; turn it off in production
+DATABASE_URL = CONFIG["DATABASE_URL"]
+engine = create_engine(DATABASE_URL, echo=True)
