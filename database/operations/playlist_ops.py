9c3907ba83e5eba272dff9ed142f1c8a7a85b308
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 20:04:42 +0600

    feat/ database ops

---
+++ database/operations/playlist_ops.py
@@ -0,0 +1,13 @@
+def add_song(user_id, song_name, song_url):
+    """Add a song to a user's playlist in the database."""
+    # TODO: Implement the database logic
+
+
+def remove_song(user_id, song_name):
+    """Remove a song from a user's playlist in the database."""
+    # TODO: Implement the database logic
+
+
+def get_playlist(user_id):
+    """Retrieve a user's playlist from the database."""
+    # TODO: Implement the database logic
