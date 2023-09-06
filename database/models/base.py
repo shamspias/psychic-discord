1f777db1ea020498171a535b2b438672221ee36a
shamspias <shamsuddinpias0@gmail.com>
2023-09-07 00:34:36 +0600

    ref base

--- database/models/base.py
+++ database/models/base.py
@@ -1,14 +1,3 @@
-import sys
-import os
 from sqlalchemy.orm import declarative_base
-from database import engine  # importing the engine
 
 Base = declarative_base()
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-if __name__ == "__main__":
-    from .user import User  # Import models to ensure they're created.
-    from .playlist import Playlist
-
-    Base.metadata.create_all(engine)
