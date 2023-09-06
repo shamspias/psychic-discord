8dbac96bb40b14f3f1c48fa3e1f8423fa8f0090e
shamspias <shamsuddinpias0@gmail.com>
2023-09-07 00:09:15 +0600

    fix: path base

--- database/models/base.py
+++ database/models/base.py
@@ -1,7 +1,11 @@
+import sys
+import os
 from sqlalchemy.ext.declarative import declarative_base
 from database import engine  # importing the engine
 
 Base = declarative_base()
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 if __name__ == "__main__":
     from .user import User  # Import models to ensure they're created.
