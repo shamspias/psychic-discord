24c2c23549af6c10ae8a8aa3872af402375538ac
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:47:50 +0600

    fix: base add all

--- database/models/base.py
+++ database/models/base.py
@@ -1,3 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
+from database import engine  # importing the engine
 
 Base = declarative_base()
+
+if __name__ == "__main__":
+    from .user import User  # Import models to ensure they're created.
+    from .playlist import Playlist
+
+    Base.metadata.create_all(engine)
