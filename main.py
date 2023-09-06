69d732189f81f64d35d580ae10dbd5c990e0e610
shamspias <shamsuddinpias0@gmail.com>
2023-09-07 03:35:35 +0600

    fix: prefix

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from discord import Intents
+import discord
+import asyncio
 from discord.ext import commands
+from config import CONFIG
 
-from config.development import CONFIG
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
 
-intents = Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=CONFIG["COMMAND_PREFIX"], intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
 
 
-bot.load_extension("cogs.music.player")
-bot.load_extension("cogs.basic_commands")
+@bot.command()
+async def test(ctx):
+    await ctx.send("Test command is working!")
+
+
+def load_cogs():
+    print("Test")
+    bot.load_extension("cogs.music.player")
+    bot.load_extension("cogs.basic_commands")
+
+
+# If you have a token in your CONFIG
 bot.run(CONFIG["BOT_TOKEN"])
