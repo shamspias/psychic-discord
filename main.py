0b1facf3f83eb59a0bcca819068e6e0b8cae085a
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 19:28:41 +0600

    config: main

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py
+
+import discord
+from discord.ext import commands
+from cogs.music.player import MusicPlayer
+from config import development as config
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+bot.add_cog(MusicPlayer(bot))
+bot.run(config.CONFIG["BOT_TOKEN"])
