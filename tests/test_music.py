2621255865c8b40787eb398ef100ca7e431ebe80
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:36:22 +0600

    feat test

---
+++ tests/test_music.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestMusic(unittest.TestCase):
+    def test_play_command(self):
+        # Implement tests for play command
+        pass
+
+    # ... other test methods for music functionality
---
+++ tests/test_playlist.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestPlaylist(unittest.TestCase):
+    def test_add_song(self):
+        # Implement tests for adding songs
+        pass
+
+    # ... other test methods for playlist functionality
