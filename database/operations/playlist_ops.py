80b7aa9717524a6dcd846774705f4b9eae6fc465
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 21:45:32 +0600

    feat playlist functions

--- database/operations/playlist_ops.py
+++ database/operations/playlist_ops.py
@@ -1,13 +1,32 @@
+from sqlalchemy.orm import sessionmaker
+from database.models.playlist import Playlist
+from database import engine
+
+Session = sessionmaker(bind=engine)
+
+
 def add_song(user_id, song_name, song_url):
     """Add a song to a user's playlist in the database."""
-    # TODO: Implement the database logic
+    session = Session()
+    new_song = Playlist(user_id=user_id, song_name=song_name, song_url=song_url)
+    session.add(new_song)
+    session.commit()
+    session.close()
 
 
 def remove_song(user_id, song_name):
     """Remove a song from a user's playlist in the database."""
-    # TODO: Implement the database logic
+    session = Session()
+    song = session.query(Playlist).filter_by(user_id=user_id, song_name=song_name).first()
+    if song:
+        session.delete(song)
+        session.commit()
+    session.close()
 
 
 def get_playlist(user_id):
     """Retrieve a user's playlist from the database."""
-    # TODO: Implement the database logic
+    session = Session()
+    songs = session.query(Playlist).filter_by(user_id=user_id).all()
+    session.close()
+    return [(song.song_name, song.song_url) for song in songs]
