5c50c23111d5a8e50848ec98c48d35222d21087a
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:50:16 +0600

    feat prod

---
+++ config/production.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+CONFIG = {
+    "BOT_TOKEN": os.getenv("BOT_TOKEN"),
+    "DATABASE_URL": os.getenv("DATABASE_URL"),
+    "YOUTUBE_API_KEY": os.getenv("YOUTUBE_API_KEY"),
+    "SPOTIFY_CLIENT_ID": os.getenv("SPOTIFY_CLIENT_ID"),
+    "SPOTIFY_CLIENT_SECRET": os.getenv("SPOTIFY_CLIENT_SECRET")
+}
