6a36461e9acd974caeec986420417edaefade7bb
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:02:49 +0600

    add mysql

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "sqlite:///./test.db"  # This is just an example using SQLite; replace with your actual database URL
+DATABASE_URL = "mysql+pymysql://username:password@localhost/dbname"  # Replace with your actual MySQL connection details
 
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True will log SQL queries; turn it off in production
