0027eaabdc2d52dec5714eb7a94ff1507b706166
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:44:44 +0600

    fix: user

--- database/models/user.py
+++ database/models/user.py
@@ -1,11 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, Sequence
+from .base import Base
 
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    username = Column(String(50))
-    discord_id = Column(String(50), unique=True)
+    discord_id = Column(String(255), unique=True, nullable=False)
+    username = Column(String(255), nullable=False)
