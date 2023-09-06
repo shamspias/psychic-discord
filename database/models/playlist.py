3b106f340399e416d6015e7a881ddc9fcec20e0c
shamspias <shamsuddinpias0@gmail.com>
2023-09-06 23:44:38 +0600

    fix: playlist

--- database/models/playlist.py
+++ database/models/playlist.py
@@ -1,15 +1,12 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Sequence
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from .user import User
-
-Base = declarative_base()
+from .base import Base
 
 
 class Playlist(Base):
     __tablename__ = 'playlists'
     id = Column(Integer, Sequence('playlist_id_seq'), primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User")  # This establishes the relationship with the User model
+    user = relationship("User")
     song_name = Column(String(255))
     song_url = Column(String(512))
